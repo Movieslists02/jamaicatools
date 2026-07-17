586a14f0fa146f823f4419d4b99516dee5478ba1
Movieslists02 <134895000+Movieslists02@users.noreply.github.com>
2026-07-17 07:23:31 +0000

    refactor: extract income tax calculation into payroll utility

--- src/components/calculators/IncomeTaxCalculator.jsx
+++ src/components/calculators/IncomeTaxCalculator.jsx
@@ -8,11 +8,10 @@ import CalculatorSummary from "../common/CalculatorSummary";
 import InlineMessage from "../common/InlineMessage";
 import ResultCard from "../common/ResultCard";
 import ToolDisclaimer from "../common/ToolDisclaimer";
+import { calculateIncomeTax } from "../../utils/payrollCalculator";
 import {
   ANNUAL_TAX_FREE_THRESHOLD,
   UPPER_TAX_BRACKET_START,
-  STANDARD_INCOME_TAX_RATE,
-  UPPER_INCOME_TAX_RATE,
 } from "../../constants/jamaicaPayroll";
 
 function IncomeTaxCalculator() {
@@ -56,40 +55,10 @@ function IncomeTaxCalculator() {
             ? enteredIncome * 52
             : enteredIncome;
 
-    const taxableIncome = Math.max(annualIncome - ANNUAL_TAX_FREE_THRESHOLD, 0);
-
-    const incomeTaxAt25Percent =
-      Math.min(taxableIncome, UPPER_TAX_BRACKET_START) *
-      STANDARD_INCOME_TAX_RATE;
-
-    const incomeTaxAt30Percent =
-      Math.max(taxableIncome - UPPER_TAX_BRACKET_START, 0) *
-      UPPER_INCOME_TAX_RATE;
-
-    const totalIncomeTax = incomeTaxAt25Percent + incomeTaxAt30Percent;
-    const afterTaxIncome = annualIncome - totalIncomeTax;
-
-    const effectiveTaxRate =
-      annualIncome > 0 ? (totalIncomeTax / annualIncome) * 100 : 0;
-
-    const marginalTaxRate =
-      taxableIncome <= 0
-        ? 0
-        : taxableIncome <= UPPER_TAX_BRACKET_START
-          ? STANDARD_INCOME_TAX_RATE * 100
-          : UPPER_INCOME_TAX_RATE * 100;
+    const taxResult = calculateIncomeTax(annualIncome);
 
     setError("");
-    setResult({
-      annualIncome,
-      taxableIncome,
-      incomeTaxAt25Percent,
-      incomeTaxAt30Percent,
-      totalIncomeTax,
-      afterTaxIncome,
-      effectiveTaxRate,
-      marginalTaxRate,
-    });
+    setResult(taxResult);
   };
 
   return (
